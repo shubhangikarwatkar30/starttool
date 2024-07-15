0b222759e45e08c423c6f02e95c523160941b3d2
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-07-15 17:15:21 +0530

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'starttooldev01.mysql.database.azure.com', 'srtadmin01', 'Ugw6h1daE5', 'startdb', 3306, MYSQLI_CLIENT_SSL);
 
 
-echo"Connection happing here";
+echo"establishing connection";
 if (mysqli_connect_errno()) {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 die('Failed to connect to MySQL: '.mysqli_connect_error());
