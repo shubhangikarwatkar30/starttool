a9a68a88a37f4e76cf55f2d147ca45584971aea2
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-07-17 16:50:11 +0530

    Update savefeedback.php

--- savefeedback.php
+++ savefeedback.php
@@ -1,25 +1,6 @@
 <?php
- include_once("config.php");
-echo "Feedback";
-if($_SERVER['REQUEST_METHOD']=='POST'){
-
-$encrypted=$_POST['d1']; 
-$password = '8R@13#s34Af';
-$method = 'aes-256-cbc';
-
-// Must be exact 32 chars (256 bit)
-$password = substr(hash('sha256', $password, true), 0, 32);
-
-// IV must be exact 16 chars (128 bit)
-$iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
-
-// av3DYGLkwBsErphcyYp+imUW4QKs19hUnFyyYcXwURU=
-//$encrypted = base64_encode(openssl_encrypt($plaintext, $method, $password, OPENSSL_RAW_DATA, $iv));
-
-//decrypt
-$decrypted = openssl_decrypt(base64_decode($encrypted), $method, $password, OPENSSL_RAW_DATA, $iv);
-
-$arraydata = explode("," ,$decrypted);
+function savefeedback($arraydata){
+include_once("config.php");
 $arr=array();
 $i=0;
 foreach( $arraydata as $d){
