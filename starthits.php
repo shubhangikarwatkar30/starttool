4b96178a58ff60cded96b0904bc0f39b4196da20
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-07-17 16:36:34 +0530

    Update starthits.php

--- starthits.php
+++ starthits.php
@@ -1,28 +1,15 @@
 
  <?php
+function savehits($arraydata){
  include_once("config.php");
 echo "executing starthits";
-if($_SERVER['REQUEST_METHOD']=='POST'){
-
-$encrypted=$_POST['d1']; 
-$password = '8R@13#s34Af';
-$method = 'aes-256-cbc';
-
-// Must be exact 32 chars (256 bit)
-$password = substr(hash('sha256', $password, true), 0, 32);
-// IV must be exact 16 chars (128 bit)
-$iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0);
-
-//decrypt the data
-$decrypted = openssl_decrypt(base64_decode($encrypted), $method, $password, OPENSSL_RAW_DATA, $iv);
- $arraydata = explode("," ,$decrypted);
-    $arr=array();
-    $i=0;
-    foreach( $arraydata as $d){
+$arr=array();
+$i=0;
+foreach( $arraydata as $d){
         $arr[$i]= explode(":",$d)[1];
     
-    $i++;
-    }
+   	 $i++;
+}
 
 
 if(strlen($decrypted)==0)
