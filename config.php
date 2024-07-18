3586346195bf5176e94d5e0cef41737bb72dc475
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-07-18 16:53:19 +0530

    Update config.php

--- config.php
+++ config.php
@@ -11,6 +11,8 @@ if (mysqli_connect_errno()) {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 exit();
+}else{
+	echo "connected to db";
 }
 
 
