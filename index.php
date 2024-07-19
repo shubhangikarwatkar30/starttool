bd525072157bc9a6c3426587affa0b311533b77a
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-07-19 17:46:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,8 +19,8 @@ $arraydata = explode("," ,$decrypted);
    savehits( $decrypted);
 $lastElement = end(explode('-', $decrypted));
  echo $lastElement;
-/*switch ($lastElement){
- case "save_hits":
+switch ($lastElement){
+/* case "save_hits":
  echo"case statement";"
   savehits( $decrypted);
  break;
@@ -33,8 +33,8 @@ $lastElement = end(explode('-', $decrypted));
   case "save_incidents":
  break;
   case "get_category":
- break;
-}*/
+ break;*/
+}
 
 }
 
