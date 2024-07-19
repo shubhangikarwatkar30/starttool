10affe9f140ba0b78c7940fbe3e846a8f58dee04
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-07-19 16:43:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0)
 $decrypted = openssl_decrypt(base64_decode($encrypted), $method, $password, OPENSSL_RAW_DATA, $iv);
 $arraydata = explode("," ,$decrypted);
 $lastElement = end(explode('-', $decrypted));
-// echo $lastElement;
+ echo $lastElement;
 savehits( $decrypted); 
 }
 
