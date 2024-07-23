d81b3fff4bafe200f7ac5ef3037a381f486bd64c
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-07-23 18:02:37 +0530

    Update savefeedback.php

--- savefeedback.php
+++ savefeedback.php
@@ -8,7 +8,7 @@ foreach( $arraydata as $d){
 
 $i++;
 }
-
+echo "in feedback";
 echo $arraydata \n;
 print_r ( $arr);
 if(strlen($arr[5])==0 || strlen($arr[2])==0 || strlen($arr[3])==0 ||strlen($arr[1])==0 || strlen($arr[0])==0)
