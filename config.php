441b35d703fbcbee7d41b5a23a0e255e0fe9b95c
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-07-25 13:30:57 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $dbname = "startdb";
 $dbuser = "srtadmin01";
 $dbpass ="Ugw6h1daE5";
 $conn = mysqli_init();
-//mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+
 mysqli_real_connect($conn, 'starttooldev01.mysql.database.azure.com', 'srtadmin01', 'Ugw6h1daE5', 'startdb', 3306, MYSQLI_CLIENT_SSL);
 
 if (mysqli_connect_errno()) {
@@ -15,10 +15,4 @@ exit();
 echo "db connected";	
 }
 
-
-//$link = @mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-//if (mysqli_connect_errno()) {
-	// echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	//exit();
-//}
 ?>
