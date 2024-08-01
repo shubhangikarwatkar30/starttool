05dce9765be70597c316219f8be2ab6386a69938
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-08-01 12:02:34 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-
+$conn = mysqli_init();
 mysqli_real_connect($conn, 'starttooldev01.mysql.database.azure.com', 'srtadmin01', 'Ugw6h1daE5', 'startdb', 3306, MYSQLI_CLIENT_SSL);
 
 if (mysqli_connect_errno()) {
