b603da83cd51468da1f28a4a579d0baef9cdadd0
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-08-01 10:51:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,9 @@ $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0)
 //decrypt the data
 $decrypted = openssl_decrypt(base64_decode($encrypted), $method, $password, OPENSSL_RAW_DATA, $iv);
 $arraydata = explode("," ,$decrypted);
-echo $decrypted;
-$lastElement =trim( end( $arraydata));
 
+$lastElement =trim( end( $arraydata));
+echo $lastElement;
 switch ($lastElement){
  case "save_hits":
    savehits( $decrypted);
