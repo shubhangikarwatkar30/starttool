35aa76bb082eb6644797f522ecf7551ff2e9cab3
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-08-01 10:45:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $iv = chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0) . chr(0x0)
 //decrypt the data
 $decrypted = openssl_decrypt(base64_decode($encrypted), $method, $password, OPENSSL_RAW_DATA, $iv);
 $arraydata = explode("," ,$decrypted);
-//echo $decrypted;
+echo $decrypted;
 $lastElement =trim( end( $arraydata));
 
 switch ($lastElement){
