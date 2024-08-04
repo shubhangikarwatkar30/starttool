03ea7c09aaca0f6d079cef1149c23c4fd8839956
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-08-04 15:31:23 +0530

    Update getcat.php

--- getcat.php
+++ getcat.php
@@ -32,9 +32,9 @@ $result = mysqli_query($conn, $Sql_Query);
          }
 	
          mysqli_close($conn);
-         $plaintext=json_encode(array($posts));
+         $plaintext=json_encode($posts);
          $encrypted = base64_encode(openssl_encrypt($plaintext, $method, $password, OPENSSL_RAW_DATA, $iv)); 
 	 $cat=trim($encrypted);
-
+	echo $cat;
        	
         }
