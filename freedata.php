11624f741e7830907ff8dad4cf853aa77e8bce58
shubhangikarwatkar30 <36227427+shubhangikarwatkar30@users.noreply.github.com>
2024-08-28 17:58:19 +0530

    Update freedata.php

--- freedata.php
+++ freedata.php
@@ -3,9 +3,9 @@ function freedata($decrypted){
 include_once("config.php");
 $arr = explode("," ,$decrypted);
 $Sql_Query = "INSERT INTO freedisk_data (user_name, execution_date, space_free) values('".$arr[0]."', now(),".$arr[1]. ")";   
-echo $Sql_Query;
+
  try {
-              echo " in try". $Sql_Query;
+              
              $result = mysqli_query($conn,$Sql_Query);
              
              if (!$result) {
